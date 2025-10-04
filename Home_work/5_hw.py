1c81d225919c17332d9b31b927c0d380a9dfaa17
Fr1zz <21.igor.com@gmail.com>
2025-10-04 21:21:56 +0300

    04.10.2025 (Домашка номер 5)

--- Home_work/5_hw.py
+++ Home_work/5_hw.py
@@ -1,4 +1,25 @@
-from selenium import webdriver 
-driver = webdriver.Chrome()
-driver.get("https://demoqa.com/")
+import time 
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+def search_elements():
+    driver.get('https://www.saucedemo.com/')
+    url = driver.current_url
+    current_title=driver.title
+    if url == 'https://www.saucedemo.com/':
+        print(url, current_title)
+    else:
+        print('ERROR')
+    Login=driver.find_element('xpath','//input[@id ="user-name"]')
+    Password=driver.find_element('xpath','//input[@id ="password"]')
+    Submit_Botton=driver.find_element('xpath','//input[@type ="submit"]')
+    if Login and Password and Submit_Botton:
+        print('Элементы найдены')
+    else:
+        print('Ошибка')
+search_elements()
 
+
+time.sleep(5)
